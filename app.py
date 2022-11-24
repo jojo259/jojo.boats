f0758cb156e73e8217a17c7f7b02ce435f985557
jojo259 <95320642+jojo259@users.noreply.github.com>
2022-11-24 15:22:20 +0000

    www. redirection

--- app.py
+++ app.py
@@ -542,6 +542,16 @@ def replaceColors(repStr):
 		print(e)
 		return ''
 
+@app.before_request # yoinked, maybe works
+def beforeRequest():
+
+	"""Redirect www requests to non-www."""
+	urlparts = urllib.parse.urlparse(request.url)
+	if urlparts.netloc == 'www.jojo.boats':
+		urlparts_list = list(urlparts)
+		urlparts_list[1] = 'jojo.boats'
+		return redirect(urllib.parse.urlunparse(urlparts_list), code=301)
+
 def prettyDate(theTime):
 	curTime = time.time()
 
