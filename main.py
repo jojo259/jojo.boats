ee3a61644f64b8e6b7c05ba1856a6072684db503
jojo259 <95320642+jojo259@users.noreply.github.com>
2022-11-24 11:57:57 +0000

    idek

--- main.py
+++ main.py
@@ -2,27 +2,7 @@
 
 import pandasocket
 
-def startFlask():
-	import app # runs itself on import (too lazy to organize)
-	# if flask is in debug mode then it errors with
-	# OSError: [WinError 10038] An operation was attempted on something that is not a socket
-	# production mode works fine
-	# no clue
-
 def startSocket():
 	pandasocket.init()
 
-if __name__ == '__main__':
-
-	print('init main')
-
-	import multiprocessing
-
-	runFlask = multiprocessing.Process(target = startFlask)
-	runSocket = multiprocessing.Process(target = startSocket)
-
-	runFlask.start()
-	runSocket.start()
-
-	runFlask.join()
-	runSocket.join()
+startSocket()
